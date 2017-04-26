680cbce6e27efc15cbcf9f8d2021b4382f92c356
KeithXi <xiaofeng.xi@net263.com>
2017-04-26 20:16:24 +0800

    add common header

---
+++ DTDBModel/DTDBModel/DTDBModel/DTDBModel.h
@@ -0,0 +1,16 @@
+//
+//  DTDBModel.h
+//  DTDBModel
+//
+//  Created by KeithXi on 26/04/2017.
+//  Copyright © 2017 KeithXi. All rights reserved.
+//
+
+#ifndef DTDBModel_h
+#define DTDBModel_h
+
+#import "DTSSQLBuilder.h"
+#import "DTBaseModel.h"
+#import "DTBaseTable.h"
+
+#endif /* DTDBModel_h */
